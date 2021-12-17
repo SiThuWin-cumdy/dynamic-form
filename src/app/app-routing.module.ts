3bb0ca65e468e742cd82954b31b58114502088a4
SiThuWin96 <winSt@corpit.com.sg>
2021-12-17 20:31:35 +0630

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormRenderComponent } from './pages/form-render/form-render.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FormRenderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
